b98642e4e7960499ee1301583f64e9e32fcd5c7d
Chris Schnaars <schnaarschris@gmail.com>
2025-02-04 10:00:50 -0500

    fixx

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./src/js/");
-    eleventyConfig.addPassthroughCopy("./src/css/");
+    eleventyConfig.addPassthroughCopy({ "./src/js/": "/" });
+    eleventyConfig.addPassthroughCopy({ "./src/css/": "/" });
 
     return {
         markdownTemplateEngine: 'njk',
